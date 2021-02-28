679178de6ffcab30f1acf88d18401dc0f6aef38b
lhzcm <lhzcm888888@gmail.com>
2021-02-28 19:34:40 +0800

    添加了发送消息是判断是否是好友

--- service/message.go
+++ service/message.go
@@ -3,10 +3,19 @@ package service
 import (
 	"errors"
 	db "photoshare/database"
+	"photoshare/models"
 	. "photoshare/models"
 )
 
 func SendMessage(msg *Message) error {
+	var count int64
+	if err := db.GormDB.Model(&models.Friend{}).
+		Where("userid = ? and friendid = ?", msg.Senderid, msg.Receiverid).Count(&count); err != nil {
+		return errors.New("消息发送失败！")
+	}
+	if count == 0 {
+		return errors.New("消息发送失败！该用户不是你的好友")
+	}
 	if err := db.GormDB.Create(msg).Error; err != nil {
 		return errors.New("消息发送失败！")
 	}
