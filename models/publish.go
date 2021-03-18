80a4c14089f909a198f7c1da693c33a326cee450
lkl <lhzcm888888@gmail.com>
2021-03-18 19:12:44 +0800

    修改了消息和文章表字段的类型由varchar=>nvarchar

--- models/publish.go
+++ models/publish.go
@@ -7,7 +7,7 @@ import (
 type Publish struct {
 	Id        int32     `gorm:"primary_key;AUTO_INCREMENT"`
 	Userid    int32     `gorm:"not null;index"`
-	Content   string    `gorm:"not null;type:varchar(2048)"`
+	Content   string    `gorm:"not null;type:nvarchar(2048)"`
 	Praise    int32     `gorm:"not null;default:0"`
 	Comments  int32     `gorm:"not null;default:0"`
 	Type      uint8     `gorm:"not null;type:tinyint;default:0"` //类型，0：只有自己看，1：好友可看，3：所有人可以看
